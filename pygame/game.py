f8a175340405842df4b58a77d6d67eb7aa273f41
Floris <FAlexandrou@uclan.ac.uk>
2019-06-24 13:16:03 +0300

    Added variety of cars, finished road/car part of the game.

--- pygame/game.py
+++ pygame/game.py
@@ -11,7 +11,7 @@ g_screenSize = width, height = 1024, 600
 class Frog:
     # Class variables
     radius = 35
-    image = pygame.image.load("pygame/frog.png")
+    image = pygame.image.load("frog.png")
     image = pygame.transform.scale(image, (radius, radius))
     rect = image.get_rect(x=width / 2 - radius, y=height - radius)
 
@@ -58,7 +58,7 @@ class Enemy(pygame.sprite.Sprite):
     radius = 70
     def __init__(self, x, y, img):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.path.join('pygame/', img))
+        self.image = pygame.image.load(os.path.join('', img))
         self.image = pygame.transform.scale(self.image, (self.radius, self.radius))
         self.rect = self.image.get_rect()
         self.rect.x = x
@@ -67,8 +67,15 @@ class Enemy(pygame.sprite.Sprite):
     def Movement(self, velocity, direction="right"):
         if direction == "right":
             self.rect = self.rect.move(velocity, 0)
+            if self.rect.x > width:
+                self.rect.x = -100
         else:
+            if self.rect.x < -100:
+                self.rect.x = width + 100
             self.rect = self.rect.move(-velocity, 0)
+
+
+
 
 
 def exitGame(event):
@@ -88,15 +95,15 @@ def main():
     # Models/Images
     screen = pygame.display.set_mode(g_screenSize)
     # Road
-    roadImage = pygame.image.load("pygame/road.png")
+    roadImage = pygame.image.load("road.png")
     roadImage = pygame.transform.scale(roadImage, (width, int(height/3)))
     road = roadImage.get_rect(x=0, y=height * 0.542)
     # Water
-    waterImage = pygame.image.load("pygame/water.png")
+    waterImage = pygame.image.load("water.png")
     waterImage = pygame.transform.scale(waterImage, (roadImage.get_width(), roadImage.get_height()))
     water = waterImage.get_rect(x=0, y=height * 0.14)
     # Grass
-    grassImage = pygame.image.load("pygame/grass.jpg")
+    grassImage = pygame.image.load("grass.jpg")
     listGrass = (grassImage.get_rect(x=0, y=height-175),
                 grassImage.get_rect(x=200, y=height-175),
                 grassImage.get_rect(x=400, y=height-175),
@@ -113,16 +120,27 @@ def main():
     clock = pygame.time.Clock()
 
     # Music
-    pygame.mixer.music.load("pygame/music.mp3")
+    pygame.mixer.music.load("music.mp3")
     pygame.mixer.music.play()
 
     # Frog Object
     objFrog = Frog()
 
     # Enemy Object
-        
-    objEnemy = Enemy(x=0, y=450, img="enemy.png")
-    objEnemy2 = Enemy(x=width, y=350, img="enemy.png")
+    listObjectEnemy = (Enemy(x=0, y=460, img="carBlack.png"),
+                       Enemy(x=400, y=460, img="carTruckRight.png"),
+                       Enemy(x=width, y=410, img="carFast.png"),
+                       Enemy(x=0, y=365, img="carWhiteRight.png"),
+                       Enemy(x=width-100, y=320, img="carTruckLeft.png"),
+                       Enemy(x=100, y=320, img="carWhiteLeft.png"))
+    # objEnemy = Enemy(x=0, y=460, img="carBlack.png")
+    # objEnemy1 = Enemy(x=400, y=460, img="carTruckRight.png")
+    #
+    # objEnemy2 = Enemy(x=0, y=365, img="carWhiteRight.png")
+    # objEnemy3 = Enemy(x=width, y=410, img="carFast.png")
+    # objEnemy4 = Enemy(x=width-100, y=310, img="carTruckLeft.png")
+    # objEnemy5 = Enemy(x=100, y=310, img="carWhiteLeft.png")
+
 
 
     # Main game loop
@@ -143,9 +161,10 @@ def main():
 
         screen.blit(waterImage, water)
         screen.blit(objFrog.image, objFrog.rect)
-       
-        screen.blit(objEnemy.image, objEnemy.rect)
-        screen.blit(objEnemy2.image, objEnemy2.rect)
+
+        for enemy in listObjectEnemy:
+            screen.blit(enemy.image, enemy.rect)
+
 
 
         # Capture all events
@@ -153,16 +172,32 @@ def main():
             exitGame(event)
             objFrog.Movement(event)
 
-        objEnemy.Movement(velocity=2, direction="right")
-        objEnemy2.Movement(velocity=1, direction="left")
+        listObjectEnemy[0].Movement(velocity=2, direction="right")
+        listObjectEnemy[1].Movement(velocity=2, direction="right")
+        listObjectEnemy[2].Movement(velocity=10, direction="left")
+        listObjectEnemy[3].Movement(velocity=3, direction="right")
+        listObjectEnemy[4].Movement(velocity=4, direction="left")
+        listObjectEnemy[5].Movement(velocity=4, direction="left")
+
+
+
+
+        # objEnemy.Movement(velocity=2, direction="right")
+        # objEnemy1.Movement(velocity=2, direction="right")
+        # objEnemy2.Movement(velocity=3, direction="right")
+        # objEnemy3.Movement(velocity=10, direction="left")
 
 
         # Collisions             
         if objFrog.rect.colliderect(water):
             print("collision")
-
-        if objFrog.rect.colliderect(objEnemy.rect):
             objFrog.ResetPosition()
+
+        for enemy in listObjectEnemy:
+            if objFrog.rect.colliderect(enemy.rect):
+                print("collision")
+                objFrog.ResetPosition()
+
 
         pygame.display.update()
 
