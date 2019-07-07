d83fa0930d8c4ec6520cd67558a9da29eadd5493
Floris <FAlexandrou@uclan.ac.uk>
2019-07-07 13:35:20 +0300

    Added a comment for clarification.

--- BeginnerExercises/SimpleWebScraper.py
+++ BeginnerExercises/SimpleWebScraper.py
@@ -5,6 +5,9 @@ url = 'https://www.nytimes.com/'
 r = requests.get(url)
 r_html = r.text
 soup = BeautifulSoup(r_html, features='html.parser')
+''' Alternative method, reduce code
+soup = BeautifulSoup.BeautifulSoup(requests.get(url).text)
+'''
 titles = soup.findAll('h2')
 for title in titles:
     print("Title: ", title.getText())
