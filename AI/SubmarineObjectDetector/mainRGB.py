4f4b701c3862245ee6c41456d875b95307e031a6
floris <falexandrou@uclan.ac.uk>
2019-09-11 14:06:13 +0200

    - Added training and validation accuracy visualization using matplotlib - Added Gaussian Noise regularization and a small amount of Dropout.

--- AI/SubmarineObjectDetector/mainRGB.py
+++ AI/SubmarineObjectDetector/mainRGB.py
@@ -1,9 +1,10 @@
 import os # used for navigating to image path
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import Dense, Dropout, ZeroPadding2D, Flatten, Conv2D, BatchNormalization, Activation, MaxPooling2D
+from keras.layers import Dense, Dropout, ZeroPadding2D, Flatten, Conv2D, BatchNormalization, Activation, MaxPooling2D, GaussianNoise
 from keras.models import Sequential, load_model
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
+from matplotlib import pyplot as plt
 
 base_path = 'garbageIMG/'
 
@@ -25,7 +26,7 @@ datagen_train = ImageDataGenerator(
     zoom_range=0.2,
     horizontal_flip=True,
     fill_mode='nearest',
-    validation_split=0.2,
+    validation_split=0.2
 )
 
 datagen_validation = ImageDataGenerator(
@@ -55,8 +56,8 @@ model.add(Conv2D(64,(3,3), padding='same', input_shape=(48, 48, 3)))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(ZeroPadding2D())
-
+model.add(GaussianNoise(0.5))
+model.add(Dropout(0.05))
 
 # 2nd Convolution layer
 model.add(Conv2D(128,(5,5), padding='same'))
@@ -64,6 +65,8 @@ model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(ZeroPadding2D())
+model.add(GaussianNoise(0.5))
+model.add(Dropout(0.05))
 
 
 # 3rd Convolution layer
@@ -71,12 +74,16 @@ model.add(Conv2D(512,(3,3), padding='same'))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(GaussianNoise(0.5))
+model.add(Dropout(0.05))
 
 # 4th Convolution layer
 model.add(Conv2D(512,(3,3), padding='same'))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(GaussianNoise(0.5))
+model.add(Dropout(0.05))
 
 model.add(Flatten())
 
@@ -84,16 +91,19 @@ model.add(Flatten())
 model.add(Dense(256))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
+model.add(GaussianNoise(0.5))
+model.add(Dropout(0.05))
 
 # Fully connected layer 2nd layer
 model.add(Dense(512))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
-
+model.add(GaussianNoise(0.5))
+model.add(Dropout(0.05))
 
 model.add(Dense(numClasses, activation='softmax'))
 opt = Adam(lr=0.0001)
-model.load_weights("weights-rgb-05-0.88.hdf5")
+model.load_weights("weights-rgb-137-0.88.hdf5")
 
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
@@ -101,12 +111,24 @@ filepath = "weights-rgb-{epoch:02d}-{val_acc:.2f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 callbacks_list = [checkpoint]
 
-model.fit_generator(generator=train_generator,
+
+training = model.fit_generator(generator=train_generator,
                     steps_per_epoch=train_generator.n//train_generator.batch_size,
                     epochs=250,
                     validation_data=validation_generator,
                     validation_steps=validation_generator.n//validation_generator.batch_size,
                     callbacks=callbacks_list,
                     verbose=2)
+
+acc = training.history['acc']
+val_acc = training.history['val_acc']
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.show()
 
 # model.save("subRGB.h5")
